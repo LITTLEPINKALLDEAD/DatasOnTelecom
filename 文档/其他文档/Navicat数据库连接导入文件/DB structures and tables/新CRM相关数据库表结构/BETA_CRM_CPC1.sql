649de7d9c027641faf13bd96452eea7cccc59904
Eason0731 <Eason.Zhang0731@outlook.com>
2019-11-01 16:43:42 +0800

    Update on 20191101

---
+++ 文档/其他文档/Navicat数据库连接导入文件/DB structures and tables/新CRM相关数据库表结构/BETA_CRM_CPC1.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 集成-仿真-CPC引擎
+Source Server Version : 50600
+Source Host           : 10.145.208.156:8544
+Source Database       : BETA_CRM_CPC1
+
+Target Server Type    : MYSQL
+Target Server Version : 50600
+File Encoding         : 65001
+
+Date: 2019-10-28 09:47:44
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
